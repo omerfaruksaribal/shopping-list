14f2ddb5119460fa261c70579bd80f8aee369bcf
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-07-01 10:47:07 +0300

    Clear buttons and Clear all button activated

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+
 
 function addItem(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
+
   // Validate Input 
   if(newItem === '') {
     alert('Please add an item');
     return;
   }
+
   // Create list Item
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
 
   const button = createButton('remove-item btn-link text-red');
+
   li.appendChild(button);
   itemList.appendChild(li);
+
   itemInput.value = '';
 }
 
@@ -35,6 +41,19 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if(e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems (e) {
+  while(itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
