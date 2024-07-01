b9d87378c02aff81512a7ac2d1a6fd8ac21e3b92
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-07-01 15:27:05 +0300

    Final of the project!

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function displayItems() {
   const itemsFromStorage = getItemsFromStorage();
@@ -19,6 +21,26 @@ function onAddItemSubmit(e) {
   if(newItem === '') {
     alert('Please add an item');
     return;
+  }
+  // Check for edit mode
+  if (isEditMode) {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists');
+      return;
+    } else {
+      const itemToEdit = itemList.querySelector('.edit-mode');
+  
+      removeItemFromStorage(itemToEdit.textContent);
+      itemToEdit.classList.remove('edit-mode');
+      itemToEdit.remove();
+      isEditMode = false;
+    }
+    
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists!');
+      return;
+    }
   }
 
   // Create item DOM element
@@ -84,8 +106,30 @@ function getItemsFromStorage() {
 function onClickItem(e) {
   if(e.target.parentElement.classList.contains('remove-item')) {
     removeItem(e.target.parentElement.parentElement); 
+  } else {
+    setItemToEdit(e.target);
   }
 }
+
+function checkIfItemExists (item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  return itemsFromStorage.includes(item);
+}
+
+function setItemToEdit(item) {
+  isEditMode = true;
+
+  itemList.querySelectorAll('li').forEach((i) => i.classList.remove('edit-mode'));
+
+  item.classList.add('edit-mode');
+  
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>  Update Item';
+  
+  formBtn.style.backgroundColor = '#228B22';
+  
+  itemInput.value = item.textContent;
+} 
 
 function removeItem(item) {
   if (confirm('Are you sure?')) {
@@ -135,8 +179,9 @@ function filterItems(e) {
   });
 }
 
-
 function checkUI () {
+  itemInput.value = '';
+
   const items = itemList.querySelectorAll('li');
   if (items.length === 0) {
     clearBtn.style.display = 'none';
@@ -145,6 +190,9 @@ function checkUI () {
     clearBtn.style.display = 'block';
     itemFilter.style.display = 'block';
   }
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add item';
+  formBtn.style.backgroundColor = '#333';
+  isEditMode = false;
 }
 // Initialize App
 function init() {
