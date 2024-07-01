c8fad721faed679e362782f714d9a133333d78ae
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-07-01 12:13:24 +0300

    Adding items to the local storage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
@@ -15,19 +15,43 @@ function addItem(e) {
     return;
   }
 
-  // Create list Item
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  // Create item DOM element
+  addItemToDOM(newItem);
 
-  const button = createButton('remove-item btn-link text-red');
-
-  li.appendChild(button);
-  // Add li to the DOM
-  itemList.appendChild(li);
+  // Add item to local storage
+  addItemToStorage(newItem);
 
   checkUI();
 
   itemInput.value = '';
+}
+
+function addItemToDOM(item) {
+    // Create list Item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+  
+    const button = createButton('remove-item btn-link text-red');
+  
+    li.appendChild(button);
+    // Add li to the DOM
+    itemList.appendChild(li);
+  
+}
+
+function addItemToStorage (item) {
+  let itemsFromStorage;
+
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+  // Add new item to array
+  itemsFromStorage.push(item);
+
+  // Conver to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function createButton(classes) {
@@ -91,7 +115,7 @@ function checkUI () {
 
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);
